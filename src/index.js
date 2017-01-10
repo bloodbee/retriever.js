8da35153b8958a3174607b9666fa895b61cbba16
Mathieu Dufour <mdufour@invivox.com>
2017-01-10 17:29:31 +0100

    default options popup, + function mouseout dom and close button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
-
-
-
-
+/**
+ * Retriever.js
+ * Author : Bloodbee (Mathieu Dufour)
+ * What does it do ? : This library display a popup/modal when the mouse of your visitor leave your website.
+ * It's useful to convert visitor into customers, by offering them a redeem coupon, or make them click on a new item link
+ **/
 
 ;(function () {
     'use strict';
-
-    var $ = require("jquery");
 
     var that;
 
@@ -17,7 +17,7 @@
         that = this;
 
         // Support instantiation without the `new` keyword.
-        if (typeof this === 'undefined' || Object.getPrototypeOf(this) !== ScrollReveal.prototype) {
+        if (typeof this === 'undefined' || Object.getPrototypeOf(this) !== Retriever.prototype) {
           return new Retriever(config);
         }
 
@@ -33,26 +33,50 @@
     */
 
     Retriever.prototype.defaults = {
-        // `<html>` is the default reveal container. You can pass either:
-        // DOM Node, e.g. document.querySelector('.fooContainer')
-        // Selector, e.g. '.fooContainer'
+        // `<html>` is the default container.
         container: window.document.documentElement,
 
         // the popup displayed when the cursor leave the container
         popup: true,
-        popupStyle : {
-            width : '100px',
-            height: '100px',
-            background-color: 'red',
+        popupStyle: {
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'center',
+            'position': 'absolute',
+            'width' : '400px',
+            'height': '250px',
+            'background-color': 'white',
+            'border': '1px solid black',
         },
 
         closeButton: true,
+        closeStyle: {
+            'background-color': 'transparent',
+            'color': 'red',
+            'font-size': '15px',
+            'font-weight': '400',
+        },
+
+        contentStyle: {
+            'text-align': 'center',
+            'font-weight': '500',
+            'font-size': '20px'
+        },
 
         link: "http://www.pykto.fr",
-        linkStyle : {
-            padding: '5px',
-            background-color: 'white',
-            color: 'black'
+        linkStyle: {
+            'padding': '5px',
+            'background-color': 'black',
+            'color': 'white',
+            'width': '200px',
+            'height': '50px',
+            'margin-left': 'auto',
+            'margin-right': 'auto',
+            'font-size': '18px',
+            'display': 'flex',
+            'align-items': 'center',
+            'justify-content': 'center',
         },
 
         // control mobile popup
@@ -62,34 +86,102 @@
     };
 
 
-    Retriever.prototype.retrieve = function(target, config) {
-        var container = window.document.documentElement; // by default it's the body
-
+    Retriever.prototype.retrieve = function(config) {
+        var conf = {};
         // initialize our retrieve
         if (config === undefined || config === null) {
-            config = {};
+            conf = this.defaults;
         } else {
-            if (target === undefined || target === null) {
-                if (config.container) container = config.container;
-            } else {
-                container = target;
-            }
+            // merge configs
+            Object.assign(conf, config, this.defaults);
         }
+
+        console.log(conf);
+
+        var container = conf.container; // by default it's the body
+
+        var popupStyle = '';
+        for (var prop in conf.popupStyle) {
+            if (conf.popupStyle.hasOwnProperty(prop))
+                popupStyle += prop+':'+conf.popupStyle[prop]+';';
+        }
+
+        var closeStyle = '';
+        for (var prop in conf.closeStyle) {
+            if (conf.closeStyle.hasOwnProperty(prop))
+                closeStyle += prop+':'+conf.closeStyle[prop]+';';
+        }
+
+        var contentStyle = '';
+        for (var prop in conf.contentStyle) {
+            if (conf.contentStyle.hasOwnProperty(prop))
+                contentStyle += prop+':'+conf.contentStyle[prop]+';';
+        }
+
+        var linkStyle = '';
+        for (var prop in conf.linkStyle) {
+            if (conf.linkStyle.hasOwnProperty(prop))
+                linkStyle += prop+':'+conf.linkStyle[prop]+';';
+        }
+
+        var link = conf.link;
 
         // Now on mouseleave container event
 
-        $(container).mouseout(function() {
-            console.log('coucou');
-        });
+        container.onmouseout = function() {
+            // remove old popup
+            var oldPopup = document.getElementById('retrieve-popup');
+            if (oldPopup)
+                document.body.removeChild(oldPopup);
+
+            // create the elements
+            var retrievePopup = document.createElement("DIV");
+            var retrieveContent = document.createElement("P");
+            var retrieveContentText = document.createTextNode("You are sure you want to go away ? We got a -25% redeem code for all website, just for you !");
+            var retrieveLinkDiv = document.createElement("SPAN");
+            var retrieveLink = document.createElement("A");
+
+            // set attributes and styles and whatever
+            retrievePopup.setAttribute('id', 'retrieve-popup');
+            retrievePopup.style.cssText = popupStyle;
+
+            retrieveContent.setAttribute('id', 'retrieve-content');
+            retrieveContent.style.cssText = contentStyle;
+
+            retrieveLinkDiv.setAttribute('id', 'retrieve-link');
+            retrieveLinkDiv.style.cssText = linkStyle;
+
+            retrieveLink.setAttribute('href', link);
+            retrieveLink.textContent = 'Click here !';
+
+            // construct the popup element
+            retrieveLinkDiv.appendChild(retrieveLink);
+            retrieveContent.appendChild(retrieveContentText);
+
+            if (conf.closeButton) {
+                var retrieveClose = document.createElement("BUTTON");
+
+                retrieveClose.setAttribute('id', 'retrieve-close');
+                retrieveClose.textContent = 'close';
+                retrieveClose.style.cssText = closeStyle;
+
+                retrievePopup.appendChild(retrieveClose);
+
+                retrieveClose.onclick = function() {
+                    document.body.removeChild(retrievePopup);
+                };
+            }
+
+            retrievePopup.appendChild(retrieveContent);
+            retrievePopup.appendChild(retrieveLinkDiv);
+
+
+            // add it to body
+            document.body.appendChild(retrievePopup);
+
+        };
 
     };
-
-
-
-
-
-
-
 
     // export our library
     if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
